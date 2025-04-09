9c1bd0c93cf80e61b844fd606f20a3621d2e0fc1
Denis Gukov <denguk@gmail.com>
2025-04-10 01:17:19 +0500

    fix(db): typo in migration

--- db/sql/migrations/v2.14.0.sql
+++ db/sql/migrations/v2.14.0.sql
@@ -2,4 +2,4 @@ alter table project__template add allow_override_branch_in_task bool not null de
 alter table `task` drop column `diff`;
 alter table `task` drop column `debug`;
 alter table `task` drop column `dry_run`;
-alter table `task` drop column `host_limit`;
+alter table `task` drop column `hosts_limit`;
