b4d0536f8acc5f303473b1f1dca719f06ed2c69c
Mark Johnson <mark@k-int.com>
2025-04-14 12:03:25 +0100

    add where to ensure idempotentency

--- db/sql/migrations/v2.14.1.sql
+++ db/sql/migrations/v2.14.1.sql
@@ -1,2 +1,2 @@
 alter table `project__integration_extract_value` add `variable_type` varchar(255) not null;
-update table `project__integration_extract_value` set `variable_type` = 'environment';
+update table `project__integration_extract_value` set `variable_type` = 'environment' where `variable_type` is null or `variable_type` = '';
