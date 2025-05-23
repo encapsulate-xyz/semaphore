d64d66997e72d843090d68f9fddbe84d6e9cf643
Denis Gukov <denguk@gmail.com>
2025-05-23 18:26:11 +0500

    ...

--- api/projects/runners.go
+++ api/projects/runners.go
@@ -52,8 +52,8 @@ func ClearRunnerCache(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetRunnerTags(w http.ResponseWriter, r *http.Request) {
-	project := context.Get(r, "project").(db.Project)
-	tags, err := helpers.Store(r).GetRunnerTags(project.ID)
+	//project := context.Get(r, "project").(db.Project)
+	tags, err := helpers.Store(r).GetRunnerTags()
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
