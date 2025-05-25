db559cb59c00643529a260e8f4037758c1ad4ad1
Denis Gukov <denguk@gmail.com>
2025-05-25 14:37:36 +0500

    fix(db): update table schema

--- db/ansible.go
+++ db/ansible.go
@@ -3,8 +3,9 @@ package db
 import "time"
 
 type AnsibleTaskHost struct {
+	ID          int       `json:"id" db:"id"`
 	TaskID      int       `json:"task_id" db:"task_id"`
-	ProjectID   int       `json:"project_id" db:"task_id"`
+	ProjectID   int       `json:"project_id" db:"project_id"`
 	Host        string    `json:"host" db:"host"`
 	Changed     int       `json:"changed" db:"changed"`
 	Failed      int       `json:"failed" db:"failed"`
@@ -17,8 +18,9 @@ type AnsibleTaskHost struct {
 }
 
 type AnsibleTaskError struct {
+	ID        int       `json:"id" db:"id"`
 	TaskID    int       `json:"task_id" db:"task_id"`
-	ProjectID int       `json:"project_id" db:"task_id"`
+	ProjectID int       `json:"project_id" db:"project_id"`
 	Host      string    `json:"host" db:"host"`
 	Task      string    `json:"task" db:"task"`
 	Error     string    `json:"error" db:"error"`
