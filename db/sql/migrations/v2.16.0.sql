48a6992e4893925d215576d3e3a5cc037ad9f5b6
Denis Gukov <denguk@gmail.com>
2025-07-01 21:35:15 +0500

    feat(keys): source column

--- db/sql/migrations/v2.16.0.sql
+++ db/sql/migrations/v2.16.0.sql
@@ -2,3 +2,5 @@ alter table `access_key` add `owner` varchar(20) default '' not null;
 alter table `access_key` add `plain` text;
 update access_key set `owner` = 'variable' where environment_id is not null and name like 'var.%';
 update access_key set `owner` = 'environment' where environment_id is not null and name like 'env.%';
+alter table `access_key` add `source_type` varchar(20) default '' not null;
+alter table `access_key` add `source_path` varchar(1000);
