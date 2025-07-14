18d98b23661cce54c90490e3f8ef3c4429896274
Denis Gukov <denguk@gmail.com>
2025-07-14 16:27:22 +0500

    feat(db): add migrations

--- db/sql/migrations/v2.16.0.sqlite.err.sql
+++
@@ -1,8 +0,0 @@
-alter table `access_key` drop `storage_id`;
-alter table `access_key` drop `source_storage_id`;
-alter table `access_key` drop `source_storage_key`;
-
-drop table project__secret_storage;
-
-alter table `access_key` drop `owner`;
-alter table `access_key` drop `plain`;
